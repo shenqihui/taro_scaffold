82d4f512b4049ed1cc9fbc1b529010c3582989bf
神奇辉 <shenqihui0920@gmail.com>
2018-07-12 20:05:42 +0800

    删除没用的代码

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
-// import { globalAdd } from './utils/debug';
 import * as dvaCore from './dva-core';
 
 const dvaApp = dvaCore.create();
+
 /* eslint-disable import/newline-after-import */
 dvaApp.model(require('./models/student').default);
 /* eslint-enable */
+
 dvaApp.start();
 const store = dvaApp._store;
 
