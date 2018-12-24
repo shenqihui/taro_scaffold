1f0ccc02ab84c7c79371fd73f97786de1185f8f2
神奇辉 <shenqihui0920@gmail.com>
2018-12-24 11:07:55 +0800

    修复 axios 的处理。

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from 'axios/dist/axios';
 import axiosAdapter from 'wepy-plugin-axios/dist/adapter';
 
 const API_URL_BASE = 'https://easy-mock.com/mock/5b44685824d4c9626905256b/taro-scaffold';
